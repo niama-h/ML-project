21eb33044fdcb11336e94bebef7973bd3bcefbd7
Viber-a <mouadmoncef5@gmail.com>
2025-01-04 22:05:44 +0100

    Update Fraud_detection.py

--- Fraud_detection.py
+++ Fraud_detection.py
@@ -33,15 +33,15 @@ df.describe()
 df[df['fraud_bool']==1]
 ###########################################
 
-exclude_column = ['credit_risk_score','device_os','source','housing_status','employment_status','payment_type']
+df = df[~((df['bank_months_count'].isna()) & (df['fraud_bool'] == 0))]
 
+exclude_column = ['credit_risk_score','device_os','source','housing_status','employment_status','payment_type']
 for col in df.columns:
     if col not in exclude_column:
         df[col] = df[col].apply(lambda x: x if x >= 0 else np.nan)
+
 df.isnull().sum()       
 df.drop(columns=['prev_address_months_count','intended_balcon_amount'], inplace=True)
-
-df = df[~((df['bank_months_count'].isna()) & (df['fraud_bool'] == 0))]
 
 numeric_df = df.select_dtypes(include=['number'])
 correlation_matrix = numeric_df.corr()
