f420b4f525661f4096054d0385313d9208d97e06
niama-h <n.hdadache@gmail.com>
2025-01-04 20:12:02 +0100

    Update Fraud_detection.py

--- Fraud_detection.py
+++ Fraud_detection.py
@@ -1,1 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ML Libraries
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+from transformers import pipeline
+from transformers import BertTokenizer
+import re
+###########################################
+file_path = r'Base.csv'
+df = pd.read_csv(file_path)
+
+###########################################
+df.size
+###########################################
+df.shape
+###########################################
+df.describe()
+###########################################
+
+numeric_df = df.select_dtypes(include=['number'])
+
+# Count the number of negative values in each numeric column
+negative_counts = numeric_df.apply(lambda x: (x < 0).sum())
+
+# Display the result
+print(negative_counts)
 
